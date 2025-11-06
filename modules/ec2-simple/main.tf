d4e15ad744c66c31effd3cfdefcd12422bed5771
Tapas Mishra <tapas.mishra@linuxfunda.com>
2025-11-06 02:23:25 +0000

    Added ingerss rule for UDP

--- modules/ec2-simple/main.tf
+++ modules/ec2-simple/main.tf
@@ -58,6 +58,15 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = var.ssh_allowed_ips
   }
 
+  # Allow WireGuard traffic (UDP) from anywhere
+  ingress {
+    from_port   = 0
+    to_port     = 60000
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow WireGuard (UDP)"
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
